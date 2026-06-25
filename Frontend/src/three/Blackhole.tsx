b17c4b84325ca80115b870123c5d351f93233207
Rishav Jha <rishavjha8515@gmail.com>
2026-06-25 13:09:03 +0530

    Update Blackhole.tsx

--- Frontend/src/three/Blackhole.tsx
+++ Frontend/src/three/Blackhole.tsx
@@ -8,7 +8,7 @@ interface BlackHoleProps {
     visual: SceneVisualConfig;
 }
 
-export function BlackHole({ visual }: BlackHoleProps) {
+export function Blackhole({ visual }: BlackHoleProps) {
     const materialRef = useRef<THREE.ShaderMaterial>(null);
     const { camera } = useThree();
 
